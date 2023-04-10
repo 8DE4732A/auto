9548a5f8d198b5969a114448fe4a7a37afd340bd
liuping <liuping@liuping.win>
2023-04-10 16:38:34 +0800

    1

--- xxxxx520.py
+++ xxxxx520.py
@@ -1,21 +1,49 @@
 import requests
 import os
 
-# index
-r = requests.get('https://www.xxxxx520.org/')
-print(r.cookies)
-cookies = {}
-cookies |= r.cookies.get_dict()
-print('cookies  ', cookies)
+def one():
+    # index
+    r = requests.get('https://www.xxxxx520.org/')
+    print(r.cookies)
+    cookies = {}
+    cookies |= r.cookies.get_dict()
+    print('cookies  ', cookies)
 
-# login
-r = requests.post('https://www.xxxxx520.org/wp-admin/admin-ajax.php', cookies=cookies,
-                  data={'action': 'user_login', 'username': 'outman', 'password': os.environ.get('PASS_WORD')})
-print(r.cookies.get_dict())
-cookies |= r.cookies.get_dict()
-print('cookies  ', cookies)
+    # login
+    r = requests.post('https://www.xxxxx520.org/wp-admin/admin-ajax.php', cookies=cookies,
+                    data={'action': 'user_login', 'username': 'outman', 'password': os.environ.get('PASS_WORD')})
+    print(r.cookies.get_dict())
+    cookies |= r.cookies.get_dict()
+    print('cookies  ', cookies)
 
-# checkout
-r = requests.post('https://www.xxxxx520.org/wp-admin/admin-ajax.php', cookies=cookies,
-                  data={'action': 'user_qiandao'})
-print(r.json())
+    # checkout
+    r = requests.post('https://www.xxxxx520.org/wp-admin/admin-ajax.php', cookies=cookies,
+                    data={'action': 'user_qiandao'})
+    print(r.json())
+
+
+def two():
+    # index
+    r = requests.get('https://xxxxx520.cam/3ajuzuo')
+    print(r.cookies)
+    cookies = {}
+    cookies |= r.cookies.get_dict()
+    print('cookies  ', cookies)
+
+    # login
+    r = requests.post('https://xxxxx520.cam/wp-admin/admin-ajax.php', cookies=cookies,
+                    data={'action': 'user_login', 'username': 'outman', 'password': 'fUpdor-8nimco-berjin'})
+    print(r.cookies.get_dict())
+    cookies |= r.cookies.get_dict()
+    print('cookies  ', cookies)
+
+    # checkout
+    r = requests.post('https://xxxxx520.cam/wp-admin/admin-ajax.php', cookies=cookies,
+                    data={'action': 'user_qiandao'})
+    print(r.json())
+
+
+if __name__ == '__main__':
+    one()
+    two()
+
